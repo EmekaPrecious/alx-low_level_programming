7b0e284803b18da6ad17037a47a869fa2ce8d9d7
EmekaPrecious <emekaprecious100@gmail.com>
2022-12-08 20:51:07 -0800

    put print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+//print output using put not printF
+
+int main(void){
+
+  puts("\"Programming is like building a multilingual puzzle, followed by a new line.");
+  return (0);
+}
