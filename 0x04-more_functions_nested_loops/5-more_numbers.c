3c612ab472cdf9cff5429d5fb2b3bde44f874225
EmekaPrecious <emekaprecious100@gmail.com>
2022-12-16 04:05:14 -0800

    more nested loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* more_numbers - print numbers 0 - 14
+* Return; 0
+*/
+
+void more_numbers(void);
+{
+int num, i;
+
+for (num = 0; num <= 9; num++)
+{
+for (i = 0; i <= 14; i++)
+_putchar('i');
+}
+_putchar('\n');
+}
