c7f18b513bac9bcf8e6b47088278f3882994f7e3
EmekaPrecious <emekaprecious100@gmail.com>
2022-12-27 00:50:41 -0800

    times table of n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,11 +8,9 @@
 
 void print_times_table(int n)
 {
-
 int a = 0, rep, b;
 if (n < 0 || n > 15)
 return;
-
 while (a <= n)
 {
 for (b = 0; b <= n; b++)
@@ -28,7 +26,7 @@ _putchar(' ');
 _putchar('0' + rep / 10);
 _putchar('0' + rep % 10);
 }
- else if (rep < 100)
+else if (rep < 100)
 {
 _putchar(' ');
 _putchar('0' + rep / 10);
@@ -46,7 +44,6 @@ _putchar(',');
 _putchar(' ');
 }
 }
-
 _putchar('\n');
 a++;
-{
+}
